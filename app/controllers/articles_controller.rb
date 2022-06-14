98a2e1cc4eec190478b1b4f32e871dde85474cfd
KevinJSS <kevin.alvarado@ucrso.info>
2022-06-14 11:56:02 -0600

    Added articles listing feature (route, controller action and index view)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,18 @@
 class ArticlesController < ApplicationController
+
+    def index
+        get_articles
+    end
+
     def show
         set_article
     end
 
     private
+    def get_articles
+        @articles = Article.all
+    end
+
     #Use callbacks to share common setup or constraint between actions
     def set_article
         @article = Article.find(params[:id])
